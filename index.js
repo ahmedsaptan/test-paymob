e50ae47d55028c4a5252565d6c83735bee8f7a7d
Ahmad <Ahmed.sayed.fcis1997@gmail.com>
2022-06-02 11:34:06 +0200

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const express= require("express");
+
+
+const app =  express();
+
+app.use(express.json());
+
+app.post("/v1/api", (req, res) => {
+    console.log(res.body);
+    res.send({
+        success: true
+    })
+})
+app.get("/v1/api", (req, res) => {
+    res.send({
+        success: true,
+        message: "API "
+    })
+})
+
+app.listen(process.env.POST, () => {
+    console.log("server is start at:", process.env.POST)
+})
